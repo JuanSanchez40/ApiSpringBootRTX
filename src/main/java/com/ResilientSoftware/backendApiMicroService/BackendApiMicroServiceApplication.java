44d215853b4aa29d66b31ab489f6ed80ed365e32
JuanSanchez40 <juanmsg34@outlook.es>
2024-10-05 16:20:42 -0700

    se agrega Dockerfile a ruta src

--- src/main/java/com/ResilientSoftware/backendApiMicroService/BackendApiMicroServiceApplication.java
+++ src/main/java/com/ResilientSoftware/backendApiMicroService/BackendApiMicroServiceApplication.java
@@ -1,16 +1,21 @@
 package com.ResilientSoftware.backendApiMicroService;
 
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
+@EnableMongoRepositories(basePackages = "com.ResilientSoftware.backendApiMicroService.repository")
 public class BackendApiMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApiMicroServiceApplication.class, args);
 	}
-
 }
