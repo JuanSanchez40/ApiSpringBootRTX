b30a620709021fbce94a44fe80b3e5b99167957a
JuanSanchez40 <juanmsg34@outlook.es>
2024-10-26 21:47:17 -0700

    se agrega localhost para poderle tirar desde el server de manera local

--- src/main/java/com/ResilientSoftware/backendApiMicroService/CorsConfig.java
+++ src/main/java/com/ResilientSoftware/backendApiMicroService/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://appsisadminx.onrender.com")
+                        .allowedOrigins("https://appsisadminx.onrender.com","http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
